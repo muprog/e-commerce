aa4b97bae3effc5b6f56e5520064a044af9b8739
muprog <128657551+muprog@users.noreply.github.com>
2025-01-12 09:43:45 +0300

    com

---
+++ app/(store)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className=''>
+      <h1>Hello world</h1>
+      <Button>click me</Button>
+    </div>
+  )
+}
